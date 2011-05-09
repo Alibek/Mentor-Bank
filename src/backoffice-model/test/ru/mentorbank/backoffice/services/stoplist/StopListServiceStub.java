013075af50b2c094fe6d135ec4383a5c45bc7ce2
Alibek <alibek.polatov@mail.ru>
2011-05-09 11:35:20 -0700

    Edited src/backoffice-model/test/ru/mentorbank/backoffice/services/stoplist/StopListServiceStub.java via GitHub

--- src/backoffice-model/test/ru/mentorbank/backoffice/services/stoplist/StopListServiceStub.java
+++ src/backoffice-model/test/ru/mentorbank/backoffice/services/stoplist/StopListServiceStub.java
@@ -11,12 +11,25 @@ public class StopListServiceStub implements StopListService {
 	public static final String INN_FOR_OK_STATUS = "1111111111111";
 	public static final String INN_FOR_STOP_STATUS = "22222222222222";
 	public static final String INN_FOR_ASKSECURITY_STATUS = "33333333333333";
+        
+        public static final String DOCUMENT_NUMBER_OK = "11111";
+	public static final String DOCUMENT_SERIAL_OK = "AAAAAA";
+	public static final String LASTNAME_OK = "POLATOV";
+	public static final String FIRSTNAME_OK = "ALIBEK";
+	public static final String MIDDLENAME_OK = "NIGMATOVICH";
 
+	public static final String DOCUMENT_NUMBER_STOP = "22222";
+	public static final String DOCUMENT_SERIAL_STOP = "BBBBB";
+	public static final String LASTNAME_STOP = "POLATOVA";
+	public static final String FIRSTNAME_STOP = "DIANA";
+	public static final String MIDDLENAME_STOP = "NIGMATOVNA";
+
+        
 	@Override
 	public StopListInfo getJuridicalStopListInfo(
 			JuridicalStopListRequest request) {
 		StopListInfo stopListInfo = new StopListInfo();
-		stopListInfo.setComment("Комментарий");
+		stopListInfo.setComment("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ");
 		if (INN_FOR_OK_STATUS.equals(request.getInn())){			
 			stopListInfo.setStatus(StopListStatus.OK);
 		} else if (INN_FOR_STOP_STATUS.equals(request.getInn())) {
@@ -29,8 +42,28 @@ public class StopListServiceStub implements StopListService {
 
 	@Override
 	public StopListInfo getPhysicalStopListInfo(PhysicalStopListRequest request) {
-		//TODO: Реализовать
-		return null;
+
+         	StopListInfo stopListInfo = new StopListInfo();
+		stopListInfo.setComment("Kommentari");
+		if (DOCUMENT_NUMBER_OK.equals(request.getDocumentNumber())
+				&& DOCUMENT_SERIAL_OK.equals(request.getDocumentSeries())
+				&& LASTNAME_OK.equals(request.getLastname())
+				&& FIRSTNAME_OK.equals(request.getFirstname())
+				&& MIDDLENAME_OK.equals(request.getMiddlename())) {
+			stopListInfo.setStatus(StopListStatus.OK);
+		} else if (DOCUMENT_NUMBER_STOP.equals(request.getDocumentNumber())
+				&& DOCUMENT_SERIAL_STOP.equals(request.getDocumentSeries())
+				&& LASTNAME_STOP.equals(request.getLastname())
+				&& FIRSTNAME_STOP.equals(request.getFirstname())
+				&& MIDDLENAME_STOP.equals(request.getMiddlename())) {
+			stopListInfo.setStatus(StopListStatus.STOP);
+		} else {
+			stopListInfo.setStatus(StopListStatus.ASKSECURITY);
+		}
+
+		return stopListInfo;
 	}
+
+
 
 }
