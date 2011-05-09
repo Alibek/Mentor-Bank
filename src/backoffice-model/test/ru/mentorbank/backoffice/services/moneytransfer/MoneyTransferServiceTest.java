55d45ca6ab39ba041314931b5027f80fa885119e
Alibek <alibek.polatov@mail.ru>
2011-05-09 11:23:41 -0700

    Edited src/backoffice-model/test/ru/mentorbank/backoffice/services/moneytransfer/MoneyTransferServiceTest.java via GitHub

--- src/backoffice-model/test/ru/mentorbank/backoffice/services/moneytransfer/MoneyTransferServiceTest.java
+++ src/backoffice-model/test/ru/mentorbank/backoffice/services/moneytransfer/MoneyTransferServiceTest.java
@@ -5,32 +5,80 @@ import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import ru.mentorbank.backoffice.dao.OperationDao;
+import ru.mentorbank.backoffice.dao.exception.OperationDaoException;
+import ru.mentorbank.backoffice.dao.stub.OperationDaoStub;
+import ru.mentorbank.backoffice.model.Operation;
+import ru.mentorbank.backoffice.model.stoplist.JuridicalStopListRequest;
+import ru.mentorbank.backoffice.model.stoplist.PhysicalStopListRequest;
+import ru.mentorbank.backoffice.model.stoplist.StopListInfo;
+import ru.mentorbank.backoffice.model.stoplist.StopListStatus;
+import ru.mentorbank.backoffice.model.transfer.AccountInfo;
+import ru.mentorbank.backoffice.model.transfer.JuridicalAccountInfo;
+import ru.mentorbank.backoffice.model.transfer.PhysicalAccountInfo;
+import ru.mentorbank.backoffice.model.transfer.TransferRequest;
+import ru.mentorbank.backoffice.services.accounts.AccountService;
+import ru.mentorbank.backoffice.services.accounts.AccountServiceBean;
 import ru.mentorbank.backoffice.services.moneytransfer.exceptions.TransferException;
+import ru.mentorbank.backoffice.services.stoplist.StopListService;
+import ru.mentorbank.backoffice.services.stoplist.StopListServiceStub;
 import ru.mentorbank.backoffice.test.AbstractSpringTest;
+import static org.mockito.Mockito.*;
 
 public class MoneyTransferServiceTest extends AbstractSpringTest {
 
 	@Autowired
 	private MoneyTransferServiceBean moneyTransferService;
+        private StopListService mockStopListService;
+	private AccountService mockAccountService;
+	private OperationDao mockOperationDao;
+	private JuridicalAccountInfo srcAccInfo;
+	private PhysicalAccountInfo dstAccInfo;
+	private TransferRequest transferRequest;
+	private StopListInfo SLI;
 
 	@Before
 	public void setUp() {
+                mockStopListService = mock(StopListServiceStub.class);
+		mockAccountService = mock(AccountServiceBean.class);
+		mockOperationDao = mock(OperationDaoStub.class);
+
+		srcAccInfo = new JuridicalAccountInfo();
+		dstAccInfo = new PhysicalAccountInfo();
+		transferRequest = new TransferRequest();
+		moneyTransferService = new MoneyTransferServiceBean();
+		SLI = new StopListInfo();
+		srcAccInfo.setInn(StopListServiceStub.INN_FOR_OK_STATUS);
+		dstAccInfo.setAccountNumber("1111111111111");
+		SLI.setStatus(StopListStatus.OK);
+		moneyTransferService.setAccountService(mockAccountService);
+		moneyTransferService.setStopListService(mockStopListService);
+		moneyTransferService.setOperationDao(mockOperationDao);
+		transferRequest.setSrcAccount(srcAccInfo);
+		transferRequest.setDstAccount(dstAccInfo);
+
 	}
 
 	@Test
 	public void transfer() throws TransferException {
-		fail("not implemented yet");
-		// TODO: Необходимо протестировать, что для хорошего перевода всё
-		// работает и вызываются все необходимые методы сервисов
-		// Далее следует закоментированная закотовка
-		// StopListService mockedStopListService =
-		// mock(StopListServiceStub.class);
-		// AccountService mockedAccountService = mock(AccountServiceBean.class);
-		//
-		// moneyTransferService.transfer(null);
-		//
-		// verify(mockedStopListService).getJuridicalStopListInfo(null);
-		// verify(mockedAccountService).verifyBalance(null);
+                
+                when(mockAccountService.verifyBalance(any(AccountInfo.class)))
+				.thenReturn(true);
+
+		when(
+				mockStopListService
+						.getJuridicalStopListInfo(any(JuridicalStopListRequest.class)))
+				.thenReturn(SLI);
+		when(
+				mockStopListService
+						.getPhysicalStopListInfo(any(PhysicalStopListRequest.class)))
+				.thenReturn(SLI);
+		moneyTransferService.transfer(transferRequest);
+		verify(mockStopListService).getJuridicalStopListInfo(
+				any(JuridicalStopListRequest.class));
+		verify(mockAccountService).verifyBalance(srcAccInfo);
+		verify(mockOperationDao).saveOperation(any(Operation.class));
+
 	}
+
 }
